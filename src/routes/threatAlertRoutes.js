cc45e03c327a61f16842e848c1ced75d9c15d536
Sajid <apple@Macbook-Pro.local>
2026-07-10 05:36:22 +0600

    feat: add PUT route for updateAlert in threatAlertRoutes

--- src/routes/threatAlertRoutes.js
+++ src/routes/threatAlertRoutes.js
@@ -9,6 +9,7 @@ router.use(analystMiddleware);
 router.post('/', alertController.createAlert);
 router.get('/', alertController.getAllAlerts);
 router.get('/:id', alertController.getAlertById);
+router.put('/:id', alertController.updateAlert);
 router.delete('/:id', alertController.deleteAlert);
 
 module.exports = router;
